c41c20433d0d238d237110a0a516af0fe99050cd
momchil-iliev <momchil.s.iliev@gmail.com>
2016-03-25 20:43:31 +0200

    Problem 1. Sum lines

---
+++ src/Problem1_SumLines.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Problem1_SumLines {
+    public static void main(String[] args) {
+        File file = new File("lines.txt");
+        String sRead;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            while ((sRead = reader.readLine()) != null) {
+                System.out.println(sumSymbols(sRead));
+            }
+        } catch (Exception ex) {
+            System.out.println("Error with file reading!");
+        }
+    }
+
+    private static int sumSymbols(String str) {
+        int sum = 0;
+        for (char ch : str.toCharArray()) {
+            sum += (int) ch;
+        }
+        return sum;
+    }
+}
